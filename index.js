c3952410fbc124e8cc2d79a75de29b6d78f148dc
Milton Sanchez <miltonsanchez5081@gmail.com>
2022-12-05 19:16:55 -0500

    complete rock, paper, scissors project

--- index.js
+++ index.js
@@ -1,50 +1,78 @@
 const options = ["rock", "paper", "scissors"];
 
-
-
-function getComputerChoice(){
-const choice = options[Math.floor(Math.random() * options.length)];
-console.log(choice);
-return choice;
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
 }
 
-
-
-
-function checkWinner(playerSelection, computerSelection){
+function checkWinner (playerSelection, computerSelection){
     if (playerSelection == computerSelection){
         return "Tie";
     }
-    else if(
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
-    ){
-        return "Player";
-    } else {
+    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+             (playerSelection == "scissors" && computerSelection == "paper") ||
+             (playerSelection == "paper" && computerSelection == "rock")) {
+    return "Player";    
+    }
+    else {
         return "Computer";
     }
 }
 
 
-
-function playRound(playerSelection, computerSelection){
- const result = checkWinner(playerSelection, computerSelection);
- if (result == "Tie"){
-    return "It's a Tie"
- }
- else if (result == "Player"){
-    return `You Win! ${playerSelection} beats ${computerSelection}`
- }
- else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
- }
+function playRound (playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == "Tie"){
+        return "It's a Tie!";
+    }
+    else if (result == "Player"){
+        return `Player Wins!, ${playerSelection} beats ${computerSelection}`;
+    }
+      else{
+          return `Computer Wins! , ${computerSelection} beats ${playerSelection}`;
+    }
 }
 
+function getPlayerChoice(){ 
+   let validatedInput = false;
+   while(validatedInput == false) {
+    const choice = prompt("Rock, Paper, or Scissors");
+    if(choice == null){
+        continue;
+    }
+    const choiceInLower = choice.toLocaleLowerCase();
+    if(options.includes(choiceInLower)){
+        validatedInput = true;
+        return choiceInLower;
+    }
+   }
+}
 
-
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    console.log("Welcome")
+for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+ console.log(playRound(playerSelection, computerSelection));
+ console.log("----------------------");
+ if(checkWinner(playerSelection, computerSelection) == "Player"){
+    playerScore++;
+ } else if(checkWinner(playerSelection, computerSelection) == "Computer"){
+    computerScore++;
+ }
+ 
+}
+console.log("Game Over")
+if(playerScore > computerScore){
+    console.log("Player Wins")
+}
+else if(playerScore < computerScore){
+    console.log("Computer Wins")
+}
+else{
+    console.log("It's a tie")
+}
+}
+game();
